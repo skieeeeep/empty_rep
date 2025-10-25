e435b4942b07a0fd4ebe8916cca55049aced5f8a
skieeeeep <89522640535e@gmail.com>
2025-10-25 14:55:20 +0300

    isPyth in progress

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+bool isPyth(unsigned, unsigned, unsigned);
 
 int main()
 {
 using_t=unsigned;
-u_t a = 0;
+u_t a = 0, b=0, c=0;
 size_t count = 0;
 std::cin >> a;
 
 while (std::cin >>a){
-count++;
+count += isPyth(a,b,c) ? 1 : 0;
 }
 if (std::cin.eof()){
 std::cout << count;
@@ -19,4 +20,11 @@ else if (std::cin.fail()){
 std::cerr << "Incorrect input\n";
 return 1
 }
+}
+
+bool isPyth(unsigned a, unsigned b, unsigned c){
+bool p = a*a == b*b + c*c;
+p = p|| b*b == a*a + c*c;
+p = p|| c*c == a*a + b*b;
+return p
 }
